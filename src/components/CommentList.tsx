624c8372bfc795173a99bb354cd15adf99895f5c
1000hyehyang <ducogus12@gmail.com>
2025-04-15 15:47:16 +0900

    fix : comment 경로 수정

--- src/components/CommentList.tsx
+++ src/components/CommentList.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Comment } from './CommentSection'
+import type { Comment } from '../api/commentApi'
 
 interface Props {
   comments: Comment[]
